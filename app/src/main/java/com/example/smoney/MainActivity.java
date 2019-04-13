1f1f0f8f9db34d17f1e64b6e820e3a3650786722
namnoit <35807995+namnoit@users.noreply.github.com>
2019-04-13 15:23:34 +0700

    First commit
    Add source files for project

---
+++ app/src/main/java/com/example/smoney/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.smoney;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private ActionBar toolbar;
+    private BottomNavigationView bottom_view;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addControls();
+        addEvents();
+    }
+
+    private void addEvents() {
+        bottom_view.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.nav_history:
+                    toolbar.setTitle("Lịch sử");
+                    loadFragment(new HistoryFragment());
+                    return true;
+                case R.id.nav_notification:
+                    toolbar.setTitle("Thông báo");
+                    loadFragment(new NotificationsFragment());
+                    return true;
+                case R.id.nav_chart:
+                    toolbar.setTitle("Biểu đồ");
+                    loadFragment(new ChartFragment());
+                    return true;
+                case R.id.nav_tax:
+                    toolbar.setTitle("Tính thuế TNCN");
+                    loadFragment(new TaxFragment());
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void addControls() {
+        toolbar = getSupportActionBar();
+        toolbar.setTitle("Lịch sử");
+        bottom_view = findViewById(R.id.bottom_view);
+        loadFragment(new HistoryFragment());
+    }
+
+    private void loadFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+
+}
