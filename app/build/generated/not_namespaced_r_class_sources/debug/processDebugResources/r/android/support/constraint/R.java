57bb8cf48d098e9c6a9bacbd46200807418f1975
phamtphuongthao <phamtphuongthao24@gmail.com>
2019-04-20 18:02:41 +0700

    select cate vs get new info

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -73,20 +73,20 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int bottom = 0x7f080025;
-        public static final int end = 0x7f080043;
-        public static final int gone = 0x7f080050;
-        public static final int invisible = 0x7f08005a;
-        public static final int left = 0x7f08005f;
-        public static final int packed = 0x7f080077;
-        public static final int parent = 0x7f080079;
-        public static final int percent = 0x7f08007c;
-        public static final int right = 0x7f080081;
-        public static final int spread = 0x7f0800a4;
-        public static final int spread_inside = 0x7f0800a5;
-        public static final int start = 0x7f0800aa;
-        public static final int top = 0x7f0800c1;
-        public static final int wrap = 0x7f0800d0;
+        public static final int bottom = 0x7f080026;
+        public static final int end = 0x7f080044;
+        public static final int gone = 0x7f080052;
+        public static final int invisible = 0x7f08005c;
+        public static final int left = 0x7f080061;
+        public static final int packed = 0x7f080079;
+        public static final int parent = 0x7f08007b;
+        public static final int percent = 0x7f08007e;
+        public static final int right = 0x7f080083;
+        public static final int spread = 0x7f0800a8;
+        public static final int spread_inside = 0x7f0800a9;
+        public static final int start = 0x7f0800ae;
+        public static final int top = 0x7f0800c6;
+        public static final int wrap = 0x7f0800d5;
     }
     public static final class styleable {
         private styleable() {}
---
+++ app/src/main/java/com/example/smoney/Get_new_info.java
@@ -0,0 +1,13 @@
+package com.example.smoney;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Get_new_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_new_info);
+    }
+}
